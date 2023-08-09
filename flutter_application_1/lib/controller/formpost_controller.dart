d83adbf9f3691fdae3cac871ea0510e8cef99661
Akarawat Panwilai <akarawat.bis@gmail.com>
2023-08-09 15:14:26 +0700

    Pilot with Separate multi orders V2005.3

--- flutter_application_1/lib/controller/formpost_controller.dart
+++ flutter_application_1/lib/controller/formpost_controller.dart
@@ -12,13 +12,14 @@ class FormPostController {
   //V101 Add Items insert: AKfycbysXkSj5PF_gSa_2_UR8EAxajHwZNJUVZgsytcOkRK7AeDDxJ7j002h1RFdf8tXPkgb6g
   //V102 V1007: AKfycbwQb8f44KKivwaVIedyqjfgjm8DdMNiobqCaeti5pb0MFOE6Ebj_QHJJY_ybrCkHBRM7w
   //V200 : AKfycbz73qwueuzA6np4hKhWziCRW6CZf9CRn_Y79rZ7a2HmAUnfZjeojgIQjDCWMbFOtoffWw เพิ่มทีละหลายชุด
+  //V203 : AKfycbzb5HmcJJPxqi2_ivR_PKSV8NShIUCp4OD4QqIxkPnYD-DpG34wsdgnUo-eHNB_etRtdg : Pilot >> V2005.2
   static const String URLApiByOne =
-      "https://script.google.com/macros/s/AKfycbz73qwueuzA6np4hKhWziCRW6CZf9CRn_Y79rZ7a2HmAUnfZjeojgIQjDCWMbFOtoffWw/exec"; // เปลี่ยนทุกครั้งที่มีการ Deploy
+      "https://script.google.com/macros/s/AKfycbzb5HmcJJPxqi2_ivR_PKSV8NShIUCp4OD4QqIxkPnYD-DpG34wsdgnUo-eHNB_etRtdg/exec"; // เปลี่ยนทุกครั้งที่มีการ Deploy
   // Success Status Message
   static const STATUS_SUCCESS = "SUCCESS";
 
-  static const String URLGoogle =
-      "https://script.google.com/macros/s/---/exec"; // API ค้นหา
+  static const String URLGetStock =
+      "https://script.google.com/macros/s/AKfycbxMT8I7oGA18NwXVG6WJCNeyUcsFk9sWqbjqEAIAtt9psQ7uOLkLW36WPBgrxcQ1OJAuQ/exec"; // API Get Stock
 
   /// Async function which saves feedback, parses [postPackForm] parameters
   /// and sends HTTP GET request on [URL]. On successful response, [callback] is called.
@@ -26,82 +27,8 @@ class FormPostController {
   void submitForm(
       PostbackForm feedbackForm, void Function(String) callback) async {
     try {
-      String txtParams = URLApiByOne +
-          "?" +
-          "val1=" +
-          DateFormat('dd-MM-yyyy').format(DateTime.now()) +
-          "&" +
-          "val2=" +
-          feedbackForm.val2 +
-          "&" +
-          "val3=" +
-          feedbackForm.val3 +
-          "&" +
-          "val4=" +
-          feedbackForm.val4 +
-          "&" +
-          "val5=" +
-          feedbackForm.val5 +
-          "&" +
-          "val6=" +
-          feedbackForm.val6 +
-          "&" +
-          "val7=" +
-          feedbackForm.val7 +
-          "&" +
-          "val8=" +
-          feedbackForm.val8 +
-          "&" +
-          "val9=" +
-          feedbackForm.val9 +
-          "&" +
-          "val10=" +
-          feedbackForm.val10 +
-          "&" +
-          "val11=" +
-          feedbackForm.val11 +
-          "&" +
-          "val12=" +
-          feedbackForm.val12 +
-          "&" +
-          "val13=" +
-          feedbackForm.val13 +
-          "&" +
-          "val14=" +
-          feedbackForm.val14 +
-          "&" +
-          "val15=" +
-          feedbackForm.val15 +
-          "&" +
-          "val16=" +
-          feedbackForm.val16 +
-          "&" +
-          "val17=" +
-          feedbackForm.val17 +
-          "&" +
-          "val18=" +
-          feedbackForm.val18 +
-          "&" +
-          "val19=" +
-          feedbackForm.val19 +
-          "&" +
-          "val20=" +
-          feedbackForm.val20 +
-          "&" +
-          "val21=" +
-          feedbackForm.val21 +
-          "&" +
-          "val22=" +
-          feedbackForm.val22 +
-          "&" +
-          "val23=" +
-          feedbackForm.val23 +
-          "&" +
-          "val24=" +
-          feedbackForm.val24 +
-          "&" +
-          "val25Post=" +
-          feedbackForm.val25Post;
+      String txtParams =
+          "$URLApiByOne?val1=${DateFormat('dd-MM-yyyy').format(DateTime.now())}&val2=${feedbackForm.val2}&val3=${feedbackForm.val3}&val4=${feedbackForm.val4}&val5=${feedbackForm.val5}&val6=${feedbackForm.val6}&val7=${feedbackForm.val7}&val8=${feedbackForm.val8}&val9=${feedbackForm.val9}&val10=${feedbackForm.val10}&val11=${feedbackForm.val11}&val12=${feedbackForm.val12}&val13=${feedbackForm.val13}&val14=${feedbackForm.val14}&val15=${feedbackForm.val15}&val16=${feedbackForm.val16}&val17=${feedbackForm.val17}&val18=${feedbackForm.val18}&val19=${feedbackForm.val19}&val20=${feedbackForm.val20}&val21=${feedbackForm.val21}&val22=${feedbackForm.val22}&val23=${feedbackForm.val23}&val24=${feedbackForm.val24}&val25Post=${feedbackForm.val25Post}";
       await http.get(Uri.parse(txtParams)).then((response) {
         Map<String, dynamic> jdata =
             convert.jsonDecode(response.body)['status'];
@@ -111,6 +38,20 @@ class FormPostController {
     } catch (e) {
       print(e);
       print(strBack);
+    }
+  }
+
+  void getStock(void Function(String) callback) async {
+    try {
+      String txtParams = URLGetStock;
+      await http.get(Uri.parse(txtParams)).then((response) {
+        Map<String, dynamic> jdata =
+            convert.jsonDecode(response.body)['status'];
+        print('Howdy, ${jdata['status']}!');
+        //var strBack = callback(convert.jsonDecode(response.body)['status']);
+      });
+    } catch (e) {
+      print(e);
     }
   }
 }
